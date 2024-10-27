1535b3a409afb0c01bcf5eef431683bc3bd67915
Arunkumar <I3518@IND-CHN-MAC1217.local>
2024-10-27 21:07:39 +0530

    youtube url added, service section modified

--- src/components/ServicesSection/ServicesSection.js
+++ src/components/ServicesSection/ServicesSection.js
@@ -15,21 +15,24 @@ function ServicesSection() {
     "Migraine",
     "Vertigo",
     "Fibromyalgia",
+  ];
+
+  const servicesGroup2 = [
     "Varicose Vein",
     "Anxiety",
     "Numbness",
     "Arthritis",
     "Joint Pain",
     "Paralysis",
-  ];
-
-  const servicesGroup2 = [
     "Knee Pain (Knee Replacement Stage)",
     "Avascular Necrosis (AVN, Hip Replacement Stage)",
     "Listhesis (Even Grade 3)",
     "Pelvic Inflammatory Disease (PID)",
     "Disc Problems (Bulge, Herniated, Prolapse, Extrusion)",
     "Uric Acid-Pain",
+  ];
+
+  const servicesGroup3 = [
     "Insomnia (Sleeplessness)",
     "Hemangioma",
     "Post Accidental Pain",
@@ -51,7 +54,7 @@ function ServicesSection() {
 
         <div className="row">
           {/* Left Column for Common Conditions */}
-          <div className="col-lg-6 mb-4">
+          <div className="col-md-4 col-sm-12 mb-4">
             <h3 className="mb-3">Common Conditions</h3>
             <ul className="list-unstyled">
               {servicesGroup1.map((service, index) => (
@@ -63,9 +66,9 @@ function ServicesSection() {
             </ul>
           </div>
 
-          {/* Right Column for Specialized Treatments */}
-          <div className="col-lg-6">
-            <h3 className="mb-3">Specialized Treatments</h3>
+          {/* Center Column for Additional Conditions */}
+          <div className="col-md-4 col-sm-12 mb-4">
+            <h3 className="mb-3">Additional Conditions</h3>
             <ul className="list-unstyled">
               {servicesGroup2.map((service, index) => (
                 <li key={index} className="d-flex align-items-center mb-2">
@@ -74,6 +77,32 @@ function ServicesSection() {
                 </li>
               ))}
             </ul>
+          </div>
+
+          {/* Right Column for Specialized Treatments */}
+          <div className="col-md-4 col-sm-12">
+            <h3 className="mb-3">Specialized Treatments</h3>
+            <ul className="list-unstyled">
+              {servicesGroup3.map((service, index) => (
+                <li key={index} className="d-flex align-items-center mb-2">
+                  <FaCheckCircle className="text-success me-2" />
+                  <span>{service}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+
+        {/* YouTube Section */}
+        <div className="text-center mt-5">
+          <div className="responsive-video">
+            <iframe
+              src="https://www.youtube.com/embed/QV0JqnKRxIc?si=iS4zj9YWm-9W9pmC"
+              title="YouTube video player"
+              style={{ border: "none" }}
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+            ></iframe>
           </div>
         </div>
       </div>
